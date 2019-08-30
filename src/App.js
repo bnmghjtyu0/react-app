efe0c390d737abc9fe634953d0214d177009ab92
RichardLiao <bnmghjtyu0@gmail.com>
2019-08-30 19:45:14 +0800

    temp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+import Home from "./home/";
+import About from "./about/";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
   );
 }
