b77ad0d302b21f569e6d863aef9b81217ac3f5b6
RichardLiao <bnmghjtyu0@gmail.com>
2019-09-04 09:42:30 +0000

    update

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ yarn add use-async-setstate
 ```
 https://www.npmjs.com/package/use-async-setstate
 https://github.com/slorber/use-async-setState
+
+### 方法二: 使用 useEffect
+https://upmostly.com/tutorials/how-to-use-the-setstate-callback-in-react
--- src/home/index.js
+++ src/home/index.js
@@ -1,4 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
-const Home = () => 'Home';
+function Home() {
+  const [age, setAge] = useState(0);
+
+
+  useEffect(() => {
+    if (age !== 0 && age >= 21) {
+      // Make API call to /beer
+    } else {
+      // Throw error 404, beer not found
+    }
+  }, [age]);
+
+  return (
+    <div>
+      <p>Drinking Age Checker</p>
+      <input
+        type="number"
+        value={age}
+        onChange={e => setAge(e.target.value)}
+      />
+    </div>
+  );
+}
+
 export default Home;
